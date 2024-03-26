4910232b02f0681db8cdc6ffa09f8faf7b3f7171
abraambenitez <thyabe77@terpmail.umd.edu>
2024-03-26 12:25:44 -0400

    Created new routes for base, contact, and favorite course while populating each template with code to display appropriate content.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,20 @@ def hello():  # put application's code here
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    print('subject: ' + request.args.get("subject"))
+    print('course_number: ' + request.args.get("course_number"))
+
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_subitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
